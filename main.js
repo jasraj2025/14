69f92f6374cc12134772193d11e5f538f4db55d7
jasraj2025 <80264078+jasraj2025@users.noreply.github.com>
2021-11-13 13:22:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshorts(){
+    save('picture.png');
+}
